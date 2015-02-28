7ee78343802d5758691ab1079ce3574cda1eb614
Andrey Trubin <andrey.trubin@logianalytics.com>
2015-02-28 17:29:24 +0200

    #1 - Added more files to application skeleton to build 'Register User ' RESTful service

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// Include our custom module
+var server = require("./server");
+var router = require("./router");
+var requestHanlders = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHanlders.start;
+handle["/start"] = requestHanlders.start;
+handle["/upload"] = requestHanlders.upload;
+handle["/show"] = requestHanlders.show;
+
+// Start the server using exported function
+server.start(router.route, handle);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+// Module function to export
+function start(route, handle) {
+	// Our request handler (I don't like anonymous functions)
+	function onRequest(request, response) {
+		var postData = "";
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received");
+		
+		request.setEncoding("utf8");
+		
+		// Data listener
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '" + postDataChunk + "'.");
+		});
 
+		// End listener
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
+	}
+
+	// Create server and listen on port 8888 
+	http.createServer(onRequest).listen("8888");
+	console.log("Server started");
+}
+
+exports.start = start;
