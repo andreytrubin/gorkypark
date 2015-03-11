b7894dc58fd06bd9a8a720418ba9735c22fc7ee8
AYasenov <yasenov95@mail.ru>
2015-03-11 23:49:23 +0200

    #5 - Fixed unit tests

--- test/spec/register/register-spec.js
+++ test/spec/register/register-spec.js
@@ -1,10 +1,31 @@
 var request = require("request");
 var fs = require("fs");
+var mysql = require("mysql");
 var SPEC_FILES = "spec/register/";
 
 function getContent(path) {
 	var result = JSON.parse(fs.readFileSync(SPEC_FILES + path, "utf8"));
 	return result;
+}
+
+function executeMySqlQuery(query, callback){
+	var connection = mysql.createConnection({
+		host: "localhost",
+		port: "3306",
+		database: "park",
+		user: "park",
+		password: "park"
+	});
+	connection.connect();
+	connection.query(query, function(err, result){
+		if(err){
+			callback(err);
+			connection.end();
+		} else{
+			callback(result);
+			connection.end();
+		}
+	});	
 }
 
 var registerAtrubin = getContent("registerAtrubin.json");
@@ -14,15 +35,22 @@ var registerInvalid = getContent("registerInvalid.json");
 var registerUrl = "http://localhost:8888/register";
 
 describe('Unregistered user processing', function() {
+	beforeEach(function(){
+//		 var post = {login: "atrubin"};
+         executeMySqlQuery("DELETE FROM user WHERE login = 'atrubin'", function(result){
+         	console.log(result);
+         });
+	});
+	
     it('Will proceed with user registration', function(done) {
         request.get(registerUrl, {json:true, body: registerAtrubin}, function(error, response, body) {
         	var registerAtrubinResponse = getContent("registerAtrubinResponse.json");
         	console.log(response.body);
-            var user = body;
+            var user = response.body;
             
         	expect(response.statusCode).toEqual(200);
-            expect(user.login).toEqual(registerAtrubinResponse.login);
-            expect(user.email).toEqual(registerAtrubinResponse.email);
+            expect(user.login).toEqual(registerAtrubinResponse.user.login);
+            expect(user.email).toEqual(registerAtrubinResponse.user.email);
             done();
         });
     });
@@ -30,13 +58,14 @@ describe('Unregistered user processing', function() {
 
 xdescribe('Registered user processing', function() {
     it('Will proceed with bad request code for registered user', function(done) {
-//    	var registerAyasenovResponse = JSON.parse(fs.readFileSync(SPEC_FILES+"registerAyasenovResponse.json", "utf8"));
-        request.get(registerUrl, {json:true, body: registerAyasenov}, function(error, response, body) {
-            expect(response.statusCode).toEqual(400);
-//            expect(response.body).toEqual(registerAyasenovResponse);
-            done();
-        });
+    	request.get(registerUrl, {json:true, body: registerAtrubin}, function(error, response, body) {
+    	console.log(response.body);
+    	expect(response.statusCode).toEqual(400);
+    	expect(response.body).toEqual("User login " + registerAtrubin.user.login + " already registered");
+        done();
+        
     });
+});
 });
 
 xdescribe('User validation', function() {
