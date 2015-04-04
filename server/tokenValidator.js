0bdeed484e2c3edbe97a261e703cbe5672b04b29
AYasenov <yasenov95@mail.ru>
2015-04-04 12:30:17 +0300

    #17 - Modified token validation

--- server/tokenValidator.js
+++ server/tokenValidator.js
@@ -5,34 +5,21 @@ var moment = require("moment");
 var models = require("./models");
 var commons = require("./commons");
 
-function validateToken(response, postData) {
-	var incomingJson = null;
+function validateToken(resonse, request) {
+
+		var header = request.headers["authorization"];
+    	var objectString = header.slice(7);
+    	var securityToken = JSON.parse(objectString);
 	
-	if (postData == "") {
-		commons.badRequest("ERROR: no content", response);
-		return;
-	} else {
-		incomingJson = commons.getJson(postData, response, commons.badRequest);
-		if (incomingJson == null) {
-			return;
-		}
-	}
-	
-	var errorsJSON = validateData(incomingJson);
-	if (errorsJSON.errors.length > 0) {
-		commons.handleError(JSON.stringify(errorsJSON), response);
-		return;
-	}
-	
-	models.User.find({where: {login: incomingJson.user}}).then(function(user){
+	models.User.find({where: {login: securityToken.user}}).then(function(user){
 		if (user != null) {
 			var time = moment().valueOf();
-			if (time >= incomingJson.expires) {
+			if (time >= securityToken.expires) {
 				commons.forbidden("Access denied", response);
 				return false;
 			} else {
-				var decoded = jwt.decode(incomingJson.token, user.salt);
-				if (decoded.iss == incomingJson.user && decoded.exp == incomingJson.expires) {
+				var decoded = jwt.decode(securityToken.token, user.salt);
+				if (decoded.iss == securityToken.user && decoded.exp == securityToken.expires) {
 					commons.success(response, {});
 					return true;
 				}
@@ -43,27 +30,5 @@ function validateToken(response, postData) {
 		}
 	});	
 }	
-
-function validateData(json) {
-	var errors = [];
-	if (json == null) {
-		errors[errors.length] = "JSON object not supplied";
-		return {
-			"errors" : errors
-		};
-	}
-	if (json.token == null || json.token == "") {
-		errors[errors.length] = "Token must not be empty";
-	}
-	if (json.expires == null || json.expires == "") {
-		errors[errors.length] = "Expires must not be empty";
-	}	
-	if (json.user == null || json.user == "") {
-		errors[errors.length] = "User must not be empty";
-	}
-	return {
-		"errors" : errors
-	};
-}
 
 exports.validateToken = validateToken;
