6b2503cfcaaa0b112324bcfa11bc333fcf104937
AYasenov <yasenov95@mail.ru>
2015-05-06 22:42:02 +0300

    #17 - Added function to calculate amount

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -10,6 +10,20 @@ module.exports = function(sequelize, DataTypes){
 			type: DataTypes.INTEGER,
 			field: "idUser",
 			foreignKey: true
+		},
+		calculateAmount: function(userLogin){
+			sequelize.query("SELECT   Cart.idUser," + 
+					"Cart.idCart, " + 
+					"sum(Attraction.price_adult*Cart_Item.adult_quant) + sum(Attraction.price_child*Cart_Item.child_quant)" + 
+					"FROM     User " + 
+					"JOIN Cart ON User.idUser = Cart.idUser" + 
+					"JOIN Cart_Item ON Cart.idCart = Cart_Item.idCart " + 
+					"JOIN Attraction ON Cart_Item.idAttraction = Attraction.idAttraction" + 
+					"WHERE    User.login = :login" + 
+					"GROUP BY Cart.idUser;", {replacements: {login: userLogin}, type: sequelize.QueryTypes.SELECT})
+					.then(function(cart) {
+						console.log(cart);
+					});
 		}
 	}, {
 		tableName: 'Cart',
